52663c5e9b7024680bd730e7d55f3cbada80d8df
Derek Day <derek.andrew.day@gmail.com>
2023-08-01 01:07:17 -0400

    mob header img

--- buckin.js
+++ buckin.js
@@ -14,7 +14,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 12000);
+  setTimeout(showSlides, 6500);
 }
 
 function reveal() {
